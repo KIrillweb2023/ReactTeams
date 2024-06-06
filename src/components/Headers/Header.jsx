9290c87b3b40f6d8571c6f8b3e7650665a13743b
Gleb <glebneshik@gmail.com>
2024-06-06 15:21:08 +0500

    React image coins ngv

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.scss";
+import { Coins } from "../../assets/img/Coins/Coins";
 
 export const Header = () => {
     return (
@@ -7,21 +8,21 @@ export const Header = () => {
                 <li className="main__wrapper_header-item">
                     <p className="main__wrapper_header-item-text">Доход от фермы:</p>
                     <div className="main__wrapper_header-item-count">
-                        <img src="/coin.svg" alt="" className="item-img-coin" />
+                        <Coins/>
                         <span className="item-text-coin">300k</span>
                     </div>
                 </li>
                 <li className="main__wrapper_header-item">
                     <p className="main__wrapper_header-item-text">До апа уровня:</p>
                     <div className="main__wrapper_header-item-count">
-                        <img src="/coin.svg" alt="" className="item-img-coin" />
+                        <Coins/>
                         <span className="item-text-coin">300k</span>
                     </div>
                 </li>
                 <li className="main__wrapper_header-item">
                     <p className="main__wrapper_header-item-text">Доход от фермы:</p>
                     <div className="main__wrapper_header-item-count">
-                        <img src="/coin.svg" alt="" className="item-img-coin" />
+                        <Coins/>
                         <span className="item-text-coin">300k</span>
                     </div>
                 </li>
